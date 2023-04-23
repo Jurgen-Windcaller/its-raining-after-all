c806d7fd942d50281f022fd24515b445f45da9c7
Archit Pandit <architpandit90@gmail.com>
2023-04-22 21:08:53 -0400

    Update PlayerClipDetection.cs
    DId that

--- Its Raining After All/Assets/Scripts/Player/PlayerClipDetection.cs
+++ Its Raining After All/Assets/Scripts/Player/PlayerClipDetection.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class PlayerClipDetection : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private int facingDir;
+
+    private void Update()
     {
-        
+        facingDir = -GetComponent<PlayerAnimation>().facing;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.tag != "____") { return; }
+
+        while (collision != null)
+        {
+            transform.Translate(new Vector3(10000, 10000, transform.position.z));
+        }
     }
 }

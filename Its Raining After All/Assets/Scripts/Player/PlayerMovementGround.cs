07ca095e9de79d12a2ce6ab29bbb37b003c8abd7
Archit Pandit <96954086+Jurgen-Windcaller@users.noreply.github.com>
2023-06-03 22:16:53 -0400

    Fixed slope movement
    Added and fixed slope movement so the player no longer falls while going on slopes, jumps when walking off a slope (sort of), or plays the falling animation when going up a slope.

--- Its Raining After All/Assets/Scripts/Player/PlayerMovementGround.cs
+++ Its Raining After All/Assets/Scripts/Player/PlayerMovementGround.cs
@@ -4,64 +4,168 @@ using UnityEngine;
 
 public class PlayerMovementGround : MonoBehaviour
 {
-    [SerializeField] private float groundSpeed = 5f;
-    [SerializeField] private float jumpForce = 5f;
-
-    [SerializeField] private LayerMask groundedMask;
-    [SerializeField] private Transform floorTransform;
-
     [HideInInspector] public Rigidbody2D rb;
 
     [HideInInspector] public bool grounded;
 
+    [SerializeField] private LayerMask groundedMask;
+
+    [SerializeField] private PhysicsMaterial2D fullFriction;
+    [SerializeField] private PhysicsMaterial2D noFriction;
+
+    [SerializeField] private float groundSpeed = 5f;
+    [SerializeField] private float jumpForce = 5f;
+    [SerializeField] private float floorCheckRad = 0.07f;
+    [SerializeField] private float slopeCheckRad = 0.25f;
+
     private Vector2 moveVec;
+    private Vector2 colSize;
+    private Vector2 slopePerpendicular;
+
+    private Vector3 floor;
+
     private Collider2D ground;
+
+    private CapsuleCollider2D playerCol;
+
+    private float slopeDownAngle;
+    private float prevSlopeDownAngle;
+    private float slopeSideAngle;
+
+    private bool onSlope;
+    private bool jumping;
 
     // Start is called before the first frame update
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        playerCol = GetComponent<CapsuleCollider2D>();
+
+        colSize = playerCol.size;
     }
 
     // Update is called once per frame
     private void Update()
     {
-        
+  
     }
 
     private void OnDrawGizmos()
     {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireSphere(floor, floorCheckRad);
+
         Gizmos.color = Color.yellow;
-        Gizmos.DrawWireSphere(floorTransform.position, 0.07f);
+        Gizmos.DrawWireSphere(floor, slopeCheckRad);
     }
 
     void FixedUpdate()
     {
         if (DialougeManager.Instance.dialougePlaying) { return; }
 
-        ground = Physics2D.OverlapCircle(floorTransform.position, 0.07f, groundedMask);
+        ground = Physics2D.OverlapCircle(floor, floorCheckRad, groundedMask);
+        floor = transform.position - new Vector3(0f, colSize.y / 2);
 
-        if (ground != null) { grounded = true; }
-        else { grounded = false; }
+        GroundCheck();
+        SlopeCheck();
 
         MovePlayer();
-
-        if (InputManager.Instance.GetJumping()) { JumpPlayer(); }
+        JumpPlayer();
     }
 
     private void MovePlayer()
     {
-        float moveVal = InputManager.Instance.GetGroundMoveRaw() * groundSpeed * Time.deltaTime;
-        moveVec = new Vector2(moveVal, rb.velocity.y);
+        float xIn = InputManager.Instance.GetGroundMoveRaw();
 
-        rb.velocity = moveVec;
+        float moveValPos = xIn * groundSpeed * Time.deltaTime;
+        float moveValNeg = -xIn * groundSpeed * Time.deltaTime;
+
+        if (grounded && !onSlope && !jumping)
+        {
+            moveVec.Set(moveValPos, 0f);
+            rb.velocity = moveVec;
+        }
+        else if (grounded && onSlope && !jumping)
+        {
+            moveVec.Set(moveValNeg * slopePerpendicular.x, moveValNeg * slopePerpendicular.y);
+            rb.velocity = moveVec;
+        }
+        else if (!grounded)
+        {
+            moveVec.Set(moveValPos, rb.velocity.y);
+            rb.velocity = moveVec;
+        }
     }
 
     private void JumpPlayer()
     {
-        if (grounded)
+        if (grounded && InputManager.Instance.GetJumping())
         {
+            jumping = true;
             rb.AddForce(Vector2.up * jumpForce * Time.deltaTime, ForceMode2D.Impulse);
+        }
+    }
+
+    private void GroundCheck()
+    {
+        if (rb.velocity.y <= 0f) { jumping = false; }
+
+        if (ground != null && !jumping) { grounded = true; }
+        else { grounded = false; }
+    }
+
+    private void SlopeCheck()
+    {
+        SlopeCheckY(floor);
+        SlopeCheckX(floor);
+    }
+
+    private void SlopeCheckX(Vector2 pos)
+    {
+        RaycastHit2D hitFront = Physics2D.Raycast(pos, transform.right, slopeCheckRad, groundedMask);
+        RaycastHit2D hitBack = Physics2D.Raycast(pos, -transform.right, slopeCheckRad, groundedMask);
+
+        if (hitFront)
+        {
+            onSlope = true;
+            slopeSideAngle = Vector2.Angle(hitFront.normal, Vector2.up);
+        }
+        else if (hitBack)
+        {
+            onSlope = true;
+            slopeSideAngle = Vector2.Angle(hitBack.normal, Vector2.up);
+        }
+        else
+        {
+            onSlope = false;
+            slopeSideAngle = 0f;
+        }
+    }
+
+    private void SlopeCheckY(Vector2 pos)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(pos, Vector2.down, slopeCheckRad, groundedMask);
+
+        if(hit)
+        {
+            slopePerpendicular = Vector2.Perpendicular(hit.normal).normalized;
+            slopeDownAngle = Vector2.Angle(hit.normal, Vector2.up);
+
+            if (slopeDownAngle != prevSlopeDownAngle) { onSlope = true; }
+
+            prevSlopeDownAngle = slopeDownAngle;
+
+            Debug.DrawRay(hit.point, hit.normal, Color.green);
+            Debug.DrawRay(hit.point, slopePerpendicular, Color.red);
+        }
+
+        if (onSlope && InputManager.Instance.GetGroundMoveRaw() == 0f)
+        {
+            rb.sharedMaterial = fullFriction;
+        }
+        else
+        {
+            rb.sharedMaterial = noFriction;
         }
     }
 }
